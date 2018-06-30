6ffc649341f212215244a9e66387e4a9b3d65cc8
Warren Tseng <wtseng678@gmail.com>
2018-06-30 10:07:43 -0700

    Impelemented htmljQueryColor

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,7 +6,29 @@ $(function() {
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  var para = document.getElementsByTagName("P");
+  for (var i = 0; i < para.length; i++) {
+  	var text = para[i].innerHTML.split(" ");
+  	for (var j = 0; j < text.length; j++) {
+  		if (text[j].length > 0) {
+  			text[j] = "<span>" + text[j] + "</span>";
+  		}
+  	}
+  	para[i].innerHTML = text.join(" ");
+  	//para[i].style.color = "red";
+  }
 
+  setInterval(function() {   
+
+  	$("span").each(function(element) { 
+	  	var r = Math.floor(Math.random() * 256);
+	  	var g = Math.floor(Math.random() * 256);
+	  	var b = Math.floor(Math.random() * 256);
+  		$(this).css("color", 'rgb(' + r + ',' + g + ',' + b + ')'); 
+  	});
+  }, 1000);
+  return para;
+  
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
