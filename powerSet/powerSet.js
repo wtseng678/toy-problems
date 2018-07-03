3a8ea6aaec4abee6de622f39e253545e28836c83
Warren Tseng <wtseng678@gmail.com>
2018-07-03 09:41:23 -0700

    Implemented powerSet

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -18,4 +18,20 @@
  */
 
 var powerSet = function(str) {
+	var result = [""];
+	for (var i = 1; i < (1 << str.length); i++) {
+		var subset = "";
+		for (var j = 0; j < str.length; j++) {
+			if (i & (1 << j)) {
+				subset += str[j];
+			}
+		}
+		result.push(subset.split("").sort().join(""));
+	}
+	return result.sort();	
 };
+
+console.log(powerSet("abc"));
+//console.log(powerSet("abc") == [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]);
+console.log(powerSet("jump"));
+//.log(powerSet("jump") == ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]);
